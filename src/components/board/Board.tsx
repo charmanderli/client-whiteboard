a0b9d1130fcd7fe1d0d3f4ca9c2f75a8ac0db732
AnnieNew <annie.niu@icloud.com>
2022-08-02 22:33:36 -0700

    can painting on the canvas now

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,8 +1,67 @@
-import React from 'react'
-import './style.css';
+import React, { useEffect } from "react";
+import "./style.css";
 
-export default function Board() {
+interface BoardProps {
+  size: number;
+  color: string;
+}
+export default function Board(props: BoardProps) {
+  useEffect(() => {
+    drawOnCanvas();
+  });
+
+  function drawOnCanvas() {
+    var canvas = document.getElementById("board") as HTMLCanvasElement;
+    var ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+    var sketch = document.getElementById("sketch") as HTMLElement;
+    var sketch_size = getComputedStyle(sketch);
+    canvas.width = parseInt(sketch_size.getPropertyValue("width"));
+    canvas.height = parseInt(sketch_size.getPropertyValue("height"));
+
+    var mouse = { x: 0, y: 0 };
+    var last_mouse = { x: 0, y: 0 };
+
+    canvas.addEventListener(
+      "mousemove",
+      function (e) {
+        last_mouse.x = mouse.x;
+        last_mouse.y = mouse.y;
+        mouse.x = e.pageX - this.offsetLeft;
+        mouse.y = e.pageY - this.offsetTop;
+      },
+      false
+    );
+
+    ctx.lineWidth = props.size;
+    ctx.lineJoin = "round";
+    ctx.lineCap = "round";
+    ctx.strokeStyle = props.color;
+
+    canvas.addEventListener("mousedown", function (e) {
+      canvas.addEventListener("mousemove", onPaint, false);
+    });
+
+    canvas.addEventListener(
+      "mouseup",
+      function () {
+        canvas.removeEventListener("mousemove", onPaint, false);
+      },
+      false
+    );
+
+    var onPaint = function () {
+      ctx.beginPath();
+      ctx.moveTo(last_mouse.x, last_mouse.y);
+      ctx.lineTo(mouse.x, mouse.y);
+      ctx.closePath();
+      ctx.stroke();
+    };
+  }
+
   return (
-    <canvas className='board' id='board'></canvas>
-  )
+    <div className="sketch" id="sketch">
+      <canvas className="board" id="board"></canvas>
+    </div>
+  );
 }
--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import Board from '../board/Board'
-import './style.css'
+import React, { useState } from "react";
+import Board from "../board/Board";
+import "./style.css";
 
 export default function Container() {
-    return (
-        <div className='container'>
-            <div className='color-picker-container'>
-                <input type="color" />
-            </div>
-            <div className='board-container'>
-                <Board></Board>
-            </div>
+  const [color, setColor] = useState("#000000");
+  const [penSize, setPenSize] = useState(5);
+  function changeColor(color: string) {
+    setColor(color);
+  }
+
+  function changeSize(size: number) {
+    setPenSize(size);
+  }
+
+  return (
+    <div className="container">
+      <div className="tools-bar">
+        <div className="color-picker-container">
+          Select Pen Color: &nbsp;
+          {/* <input type="color" value={color} onChange={changeColor}/> */}
         </div>
-    )
+      </div>
+
+      <div className="board-container">
+        <Board color="#000000" size={5}></Board>
+      </div>
+    </div>
+  );
 }
-
-
